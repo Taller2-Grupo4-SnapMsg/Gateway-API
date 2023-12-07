bcab2e3fb98605114beab600160af57b5ef5bdb0
lara-scazzola1 <lscazzola@fi.uba.ar>
2023-12-07 16:45:38 -0300

    se arreglo ruta

--- control/users/users.py
+++ control/users/users.py
@@ -205,13 +205,14 @@ def login_with_biometrics(biometric_token: str = Header(...)):
     )
     return generate_response(response)
 
+
 @router.get("/users/username/{username}")
-def get_user_by_username(username: str, token: str = Header(...)):
+def get_user_by_username_from_user(username: str, token: str = Header(...)):
     """
     Get a user either by email or by username
     """
     headers_request = create_header_token(token)
-    url = f"{USERS_URL}/users/{username}"
+    url = f"{USERS_URL}/users/username/{username}"
 
     response = requests.get(
         url, headers=headers_request, timeout=TIMEOUT
